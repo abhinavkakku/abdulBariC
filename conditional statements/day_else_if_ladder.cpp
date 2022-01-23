0db6bee38bd56ec98615f7c67d838290fd597dd3
Abhinav Kumar <abhinav.kakku@gmail.com>
2022-01-24 00:52:29 +0530

    if else ladder day

---
+++ conditional statements/day_else_if_ladder.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int day;
+    day = 6;
+    if(day ==1){
+        cout<<"It's Monday"<<endl;
+    }
+    else if( day ==2){
+        cout<<"It's Tuesday"<<endl;
+    }
+    else if( day ==3){
+        cout<<"It's Wednesday"<<endl;
+    }
+    else if( day ==4){
+        cout<<"It's Thursday"<<endl;
+    }
+    else if( day ==5){
+        cout<<"It's Friday"<<endl;
+    }
+    else if( day ==6){
+        cout<<"It's Saturday"<<endl;
+    }
+    else if( day ==7){
+        cout<<"It's Sunday"<<endl;
+    }
+    return 0;
+}

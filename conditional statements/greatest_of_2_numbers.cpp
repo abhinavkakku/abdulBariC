cae9c10aa5bc25090cdc0c53cb0d9ef8c26ac518
Abhinav Kumar <abhinav.kakku@gmail.com>
2022-01-23 09:25:37 +0530

    conditional statements

---
+++ conditional statements/greatest_of_2_numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Enter two numbers to x & y"<<endl;
+    x=10;
+    y = 15;
+    if (x>y){
+        cout<<"X is greater than y"<<endl;
+    }
+    else{
+        cout<<"X is less than Y, Y is bigger"<<endl;
+    }
+    return 0;
+}
---
+++ conditional statements/if_else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    if (true){
+        cout<<"True exexuted"<<endl;
+    }
+    else{
+        cout<<"Else false executed"<<endl;
+    }
+    return 0;
+}
